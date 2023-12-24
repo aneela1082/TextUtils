2c634c43b8d6d8202b76f5e65d5887add256cefa
aneela1082 <aneelasiddiqui1@gmail.com>
2023-12-24 19:31:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert,setAlert]= useState(null);
+  
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type:type
+    })
+  }
+
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark')
+      //document.body.style.backgroundColor = '#042743'
+      //showAlert("Dark mode enabled","success");
+      document.body.style.background =
+        "linear-gradient(45deg, rgb(92, 92, 92) 50%, rgb(27, 27, 27) 50%)";
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light')
+      //document.body.style.backgroundColor = 'white'
+      document.body.style.background =
+        "linear-gradient(45deg, rgb(241, 241, 241) 50%, rgb(158, 232, 255) 50%)";
+      showAlert("Light mode has been enabled", "success");
+      //showAlert("Light mode enabled","success");
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+      <TextForm heading="Enter your text here:" mode={mode}/>
+      {/* <About/> */}
     </div>
+    </>
   );
 }
 
